d8d32e4b07c3ff46e5106114df44c85bced4423c
Fly596 <69361885+Fly596@users.noreply.github.com>
2025-07-25 18:27:13 -0500

    .

--- app/src/main/java/com/example/effectivemobiletask/features/coursedetails/CourseDetailsScreen.kt
+++ app/src/main/java/com/example/effectivemobiletask/features/coursedetails/CourseDetailsScreen.kt
@@ -2,13 +2,16 @@ package com.example.effectivemobiletask.features.coursedetails
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.effectivemobiletask.R
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun CourseDetailsScreen(
@@ -17,17 +20,27 @@ fun CourseDetailsScreen(
 ) {
     val state by viewModel.uiState.collectAsStateWithLifecycle()
 
-    TopSection()
+    TopSection(
+        rate = state.course?.rate.toString(),
+        publishDate =
+            state.course
+                ?.publishDate
+                ?.format(DateTimeFormatter.ofPattern("MMMM d, yyyy")),
+    )
 }
 
 @Composable
-fun TopSection(rate: String,
-               publishDate: String,
-               image: Int = R.drawable.course_picture_default ) {
+fun TopSection(
+    rate: String = "9.9",
+    publishDate: String? ,
+    image: Int = R.drawable.course_picture_default,
+) {
     Box(modifier = Modifier) {
         Image(
             painter = painterResource(R.drawable.course_picture_default),
             contentDescription = null,
+            contentScale = ContentScale.Crop,
         )
+        Text(rate)
     }
 }
